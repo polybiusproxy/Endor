9b477005ad0386b5bb62453f2a2dc393b57a20bf
expLOIT <herotutos89@gmail.com>
2021-07-01 20:03:33 +0200

    Added testing files

---
+++ Endor/src/testlmao.cpp
@@ -0,0 +1,11 @@
+#include "testlmao.h"
+#include <stdio.h>
+
+// this is useless and its only for testing lol
+namespace Endor
+{
+	void Print()
+	{
+		printf("Welcome to Endor!\n");
+	}
+}
---
+++ Endor/src/testlmao.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// this is useless and its only for testing lol
+namespace Endor
+{
+	_declspec(dllexport) void Print();
+}
