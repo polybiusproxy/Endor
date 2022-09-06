9a7eece90fae7f1daeca9f531d085c0a3b03c270
Dan Roberts <47796739+polybiusproxy@users.noreply.github.com>
2022-09-06 18:39:24 +0200

    Fix GLAD.h backend

--- src/glad/GLAD.hx
+++ src/glad/GLAD.hx
@@ -2,7 +2,7 @@ package glad;
 
 @:keep
 @:include("glad/glad.h")
-@:buildXml("C:/Users/Win32/Desktop/Programming/Haxe/Endor/src/glad/glad.xml")
+@:buildXml("<include name='C:/Users/Win32/Desktop/Programming/Haxe/Endor/src/glad/glad.xml'/>")
 extern class GLAD
 {
 	@:native("gladLoadGL")
